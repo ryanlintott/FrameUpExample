a9cb1ea915992769a3f968400ddbe09ba82f8e1e
Ryan Lintott <ryan.lintott@abetterwaytodo.com>
2021-09-14 15:38:46 -0400

    Initial Commit

---
+++ FrameUpExample/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FrameUpExample
+//
+//  Created by Ryan Lintott on 2021-09-14.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
