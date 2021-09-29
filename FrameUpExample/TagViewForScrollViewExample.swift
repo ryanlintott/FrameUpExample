2117acce2b3cf7bc16ec7caa854b3c3a0dd5850b
Ryan Lintott <ryan.lintott@abetterwaytodo.com>
2021-09-29 18:21:38 -0400

    Fixed SmartScrollView bug when optionalScrolling was off Fixed TagView to accounts for NavigationView and other alignment changes.

--- FrameUpExample/TagViewForScrollViewExample.swift
+++ FrameUpExample/TagViewForScrollViewExample.swift
@@ -38,6 +38,8 @@ struct TagViewForScrollViewExample: View {
 
 struct TagViewForScrollViewExample_Previews: PreviewProvider {
     static var previews: some View {
-        TagViewForScrollViewExample()
+        NavigationView {
+            TagViewForScrollViewExample()
+        }
     }
 }
