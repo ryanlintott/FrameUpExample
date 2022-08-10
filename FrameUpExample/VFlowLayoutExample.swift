fae35aed2022336cea1b9b573e707bb6b6f853cf
Ryan Lintott <ryan.lintott@abetterwaytodo.com>
2022-08-10 12:10:49 -0400

    Changed alignment properties in HFlow and VFlow Changed default alignment in ZStack to center Updated VFlowLayoutExample Commented out ForEach and _View extensions for all Layouts

--- FrameUpExample/VFlowLayoutExample.swift
+++ FrameUpExample/VFlowLayoutExample.swift
@@ -9,37 +9,83 @@ import FrameUp
 import SwiftUI
 
 struct VFlowLayoutExample: View {
-    
-    @State private var maxWidth: CGFloat = 300
-    
+    var body: some View {
+        TabView {
+            VFlowLayoutForEachExample()
+                .tabItem {Label("ForEach", systemImage: "list.dash") }
+            
+            VFlowLayoutForEachExample()
+                .tabItem {Label("_View", systemImage: "rectangle.3.group") }
+        }
+    }
+}
+
+struct VFlowLayoutForEachExample: View {
     let items = ["FULayout", "Layup", "Formation", "Board", "Gang", "Herd", "Hord", "Swarm", "Arrangement", "Exhibit", "Blueprint", "Collage"]
         .map { Item(id: UUID(), value: $0) }
     
+    @State private var maxWidth: CGFloat = 500
+    
     var body: some View {
         VStack {
-            VFlowLayout(maxWidth: maxWidth)._view {
-                ForEach(items) { item in
+            WidthReader { width in
+                VFlowLayout(maxWidth: width).forEach(items) { item in
                     Text(item.value)
                         .padding()
                         .foregroundColor(.white)
                         .background(Capsule().fill(.blue))
                 }
+                .background(Color.gray)
+                .animation(.spring(), value: maxWidth)
+                
             }
-            .animation(.spring(), value: maxWidth)
-            .onTapGesture {
-                maxWidth -= 50
-            }
+            .frame(maxWidth: maxWidth)
             
-            VFlowLayout(maxWidth: maxWidth).forEach(items) { item in
-                Text(item.value)
-                    .padding()
-                    .foregroundColor(.white)
-                    .background(Capsule().fill(.blue))
+            Spacer()
+            HStack {
+                Text("Max Width")
+                Slider(value: $maxWidth, in: 50...600) {
+                    Text("Max Width")
+                }
             }
-            .animation(.spring(), value: maxWidth)
-            .onTapGesture {
-                maxWidth += 50
+            .padding()
+        }
+    }
+}
+
+struct VFlowLayout_ViewExample: View {
+    let items = ["FULayout", "Layup", "Formation", "Board", "Gang", "Herd", "Hord", "Swarm", "Arrangement", "Exhibit", "Blueprint", "Collage"]
+        .map { Item(id: UUID(), value: $0) }
+    
+    @State private var maxWidth: CGFloat = 300
+    
+    var body: some View {
+        VStack {
+            SmartScrollView([.vertical], optionalScrolling: true, shrinkToFit: true) {
+                WidthReader { width in
+                    VFlowLayout(
+                        maxWidth: width)._view {
+                            ForEach(items) { item in
+                                Text(item.value)
+                                    .padding()
+                                    .foregroundColor(.white)
+                                    .background(Capsule().fill(.blue))
+                            }
+                        }
+                        .background(Color.gray)
+                        .animation(.spring(),
+                                   value: maxWidth)
+                }
+                .frame(maxWidth: maxWidth)                
             }
+            Spacer()
+            HStack {
+                Text("Max Width")
+                Slider(value: $maxWidth, in: 50...600) {
+                    Text("Max Width")
+                }
+            }
+            .padding()
         }
     }
 }
