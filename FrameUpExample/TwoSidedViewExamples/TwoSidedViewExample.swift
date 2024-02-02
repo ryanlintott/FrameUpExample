835da6f5f31e56e31e196d807abdc04257d7df17
Ryan Lintott <ryanlintott@users.noreply.github.com>
2024-02-02 15:01:14 -0500

    Updated visionOS icon Adjustments to TwoSidedViewExample for visionOS

--- FrameUpExample/TwoSidedViewExamples/TwoSidedViewExample.swift
+++ FrameUpExample/TwoSidedViewExamples/TwoSidedViewExample.swift
@@ -12,17 +12,33 @@ struct TwoSidedViewExample: View {
     @State private var angle: Angle = .zero
     @State private var axis: Axis = .horizontal
     
+    var rotationAxis: (x: CGFloat, y: CGFloat, z: CGFloat) {
+        axis == .horizontal ? (0,1,0) : (1,0,0)
+    }
+    
+    var backView: some View {
+        RoundedRectangle(cornerRadius: 20)
+            .fill(.red)
+            .overlay(Text("Down"))
+    }
+    
     var body: some View {
         VStack {
             RoundedRectangle(cornerRadius: 20)
                 .fill(.blue)
                 .overlay(Text("Up"))
+                #if os(visionOS)
+                .rotation3DEffect(angle, axis: axis == .horizontal ? .y : .x) {
+                    backView
+                }
+                .offset(z: 100)
+                .frame(width: 200, height: 200)
+                #else
                 /// This modifier creates the two-sided view by supplying a rotation and a back side.
                 .rotation3DEffect(angle, axis: axis == .horizontal ? (0,1,0) : (1,0,0), perspective: 0.5) {
-                    RoundedRectangle(cornerRadius: 20)
-                        .fill(.red)
-                        .overlay(Text("Down"))
+                    backView
                 }
+                #endif
                 .padding()
             
             Picker("Axis", selection: $axis) {
@@ -34,7 +50,7 @@ struct TwoSidedViewExample: View {
             
             Text("Change Rotation")
             HStack {
-                ForEach([-360,-180,-120,-45,45,120,180,360], id: \.self) { i in
+                ForEach([-360,-180,-90,-45,45,90,180,360], id: \.self) { i in
                     Button("\(i > 0 ? "+" : "")\(i)") {
                         withAnimation(.spring().speed(0.4)) {
                             angle += .degrees(Double(i))
